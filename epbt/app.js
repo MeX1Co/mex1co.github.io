66be7926c48959aab290e3842bbc91cc50eef584
MeX1Co <gmdoukas@yahoo.com>
2024-08-23 10:24:39 +0300

    Update app.js

--- epbt/app.js
+++ epbt/app.js
@@ -118,7 +118,9 @@ function stopAudio() {
 }
 
 function updateScreen(index, isStopped = false) {
-    document.getElementById('line2').textContent = isStopped ? 'Now Playing: ' : `Now Playing: ${songOptions[currentSong].title} ${String(index).padStart(2, '0')}`;
+    document.getElementById('line2').textContent = isStopped
+        ? 'Now Playing: '
+        : `Now Playing: ${songOptions[currentSong].files[index].split('/').pop()}`;
     document.getElementById('line3').textContent = isStopped ? '' : (songOptions[currentSong].loopFiles.includes(index) ? 'LOOPING' : 'NOT LOOPING');
     document.getElementById('line3').style.color = songOptions[currentSong].loopFiles.includes(index) ? 'var(--orange)' : 'var(--white)';
 }
